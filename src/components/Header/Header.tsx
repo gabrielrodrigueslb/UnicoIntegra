021511cec2f868801444999f02148ccfb69cda58
gabrielrodrigueslb <gbri2586@gmail.com>
2025-10-27 14:49:29 -0300

    Fix: Nav button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,12 +27,14 @@ export default function Header() {
         <nav className="navbar flex flex-col justify-between h-full items-end">
           <ul className="nav-list flex flex-col gap-3">
             {menuItems.map((item, index) => (
+              <Link to={item.path}>
               <li
                 className={`p-3.5 bg-gray-950 text-white rounded-full text-2xl cursor-pointer ${item.path == location.pathname ? "active": ""} hover:bg-(--color-primary)`}
                 key={index}
               >
-                <Link to={item.path}>{item.icon}</Link>
+               {item.icon}
               </li>
+              </Link>
             ))}
           </ul>
           <button
